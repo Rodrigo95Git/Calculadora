9fbdfd28f40ccde571ffbecd170d476bb0353223
RodrigoGit <rodsuve@gmail.com>
2025-04-21 22:24:34 -0600

    Âcrea instancia de la clase Operaciones en el main y se llama al metodo suma

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Hello world!");
+        double n1=25, n2=5 , resulsuma;
+        Operaciones suma=new Operaciones();
+        resulsuma=suma.suma(n1,n2);
+        System.out.println("La suma es : " + resulsuma);
     }
 }
