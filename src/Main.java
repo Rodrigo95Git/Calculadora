da26a017eb11a52b2aaea2ba721b674494dc32d6
RodrigoGit <rodsuve@gmail.com>
2025-04-21 22:31:31 -0600

    se crea el metodo resta se manda llamar en la clase main

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        double n1=25, n2=5 , resulsuma;
+        double n1=25, n2=5 , resulsuma, ressulresta;
         Operaciones suma=new Operaciones();
         resulsuma=suma.suma(n1,n2);
         System.out.println("La suma es : " + resulsuma);
+
+        Operaciones resta=new Operaciones();
+        ressulresta=resta.resta(n1,n2);
+        System.out.println("La resta es : " + ressulresta);
     }
 }
--- src/Operaciones.java
+++ src/Operaciones.java
@@ -4,4 +4,8 @@ public class Operaciones {
         return n1+n2;
 
     }
+    public double resta(double n1, double n2){
+        return n1-n2;
+
+    }
 }
